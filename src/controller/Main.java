a8010bcf92419fadea0a203b1bfcc723f79a0fc8
PrashanthiRamesh <rvprashanthi@gmail.com>
2019-01-30 15:22:12 -0500

    - menu test commit

---
+++ src/controller/Main.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class Main {
+
+	public static void main(String args[]){
+		System.out.println("Get Ready to play Conquest!\n");
+		GameMenu gameMenu=new GameMenu();
+		gameMenu.display();
+		gameMenu.select();
+	}
+	
+	
+}
