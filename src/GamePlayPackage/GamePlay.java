98cd68117fdb1ba4ce3b8b94270c5ef4eda9a6ba
Suthakhar <psuthakhar@gmail.com>
2019-02-27 14:54:36 -0500

    - GamePlay.java class created

---
+++ src/GamePlayPackage/GamePlay.java
@@ -0,0 +1,41 @@
+package GamePlayPackage;
+
+import java.util.ArrayList;
+
+import PlayerPackgae.PlayerClass;
+
+public class GamePlay {
+
+	
+	public void reinforcementphase(ArrayList<PlayerClass> players)
+	{
+		
+		
+		
+		
+	}
+	
+	
+	public void attackphase(ArrayList<PlayerClass> players)
+	{
+		
+		
+		
+		
+		
+	}
+	
+	
+	public void  fortificationsphase(ArrayList<PlayerClass> players)
+	{
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}
