7280496293e7d46f3f1d74405f60aa3823ef71e5
Mutesham <sardarms9@gmail.com>
2019-02-27 15:03:15 -0500

    - Country.java class created- yet to be tested

---
+++ src/CountryPackage/CountryClass.java
@@ -0,0 +1,38 @@
+package CountryPackage;
+
+public class CountryClass {
+
+	String countryName;
+	String belongsTo;
+	int armies;
+	public CountryClass(String countryName, String belongsTo, int armies) {
+		this.countryName = countryName;
+		this.belongsTo = belongsTo;
+		this.armies = armies;
+	}
+	public String getCountryName() {
+		return countryName;
+	}
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+	public String getBelongsTo() {
+		return belongsTo;
+	}
+	public void setBelongsTo(String belongsTo) {
+		this.belongsTo = belongsTo;
+	}
+	public int getArmies() {
+		return armies;
+	}
+	public void setArmies(int armies) {
+		this.armies = armies;
+	}
+	@Override
+	public String toString() {
+		return "CountryClass [countryName=" + countryName + ", belongsTo=" + belongsTo + ", armies=" + armies + "]";
+	}
+	
+	
+	
+}
